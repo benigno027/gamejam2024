634fb8419484e5501208ee19f7ef13bfbd06a369
joseph <josephxax1997@hotmail.com>
2024-01-27 15:57:04 -0500

    cambios

--- Assets/Scripts/MiniGame1Scripts.cs
+++ Assets/Scripts/MiniGame1Scripts.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.UI;
+using System.Collections;
 
 public class MiniGame1Scripts : MonoBehaviour
 {
@@ -15,13 +16,49 @@ public class MiniGame1Scripts : MonoBehaviour
 
         for (int i = 0; i < Recorridos.Length; i++)
         {
-            Recorridos[i] = Random.Range(0, gameObjectsButtons.Length);
+            Recorridos[i] = UnityEngine.Random.Range(0, gameObjectsButtons.Length) - 1;
             Debug.Log(Recorridos[i]);
+
+        }
+        StartCoroutine(ButtonSignal());
+    }
+
+    public void ComprobarFoco(int indice)
+    {
+        //verificar si el indice existe en el array de recorridos
+        int existe = System.Array.IndexOf(Recorridos, indice);
+
+        if(existe != -1){
+            Debug.Log("Existe");
+        }
+        else{
+            Debug.Log("No existe");
         }
     }
 
-    void Update()
+    IEnumerator ButtonSignal()
     {
+        
+        for (int i = 0; i < Recorridos.Length; i++)
+        {
+            int idx = Recorridos[i];
+
+            SetButtonColor(idx, Color.red);
+            yield return new WaitForSeconds(0.5f);
+
+            SetButtonColor(idx, Color.white);
+            yield return new WaitForSeconds(0.5f);
+        }
+    }
+
+    void SetButtonColor(int idx, Color color)
+    {
+        Debug.Log("idx");
+        Debug.Log(idx);
+        Button button = gameObjectsButtons[idx].GetComponent<Button>();
+        ColorBlock colors = button.colors;
+        colors.normalColor = color;
+        button.colors = colors;
     }
 }
 
