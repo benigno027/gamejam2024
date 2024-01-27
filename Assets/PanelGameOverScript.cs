9ab4cdcff34b93a7d26a153331ab9b211997fa9d
“Legna31” <angeluis3101@gmail.com>
2024-01-27 12:41:45 -0500

    Panel Gameover

---
+++ Assets/PanelGameOverScript.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelGameOverScript : MonoBehaviour
+{
+    public GameObject image1; 
+    public GameObject image2;
+    public GameObject image3;
+    public Transform UbicionFinal1;
+    public Transform UbicionFinal2;
+    public Transform UbicionFinal3;
+    
+    public float movespeed = 0.5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveImage(image1, UbicionFinal1);
+        MoveImage(image2, UbicionFinal2);
+        MoveImage(image3, UbicionFinal3);
+
+    }
+    void MoveImage(GameObject image , Transform target)
+    {
+        // Move our position a step closer to the target.
+
+        float step =  movespeed * Time.deltaTime; // calculate distance to move
+        image.transform.position = Vector3.MoveTowards(image.transform.position, target.position, step);
+        
+    }
+}
